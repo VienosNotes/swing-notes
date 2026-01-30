72293f43e8b1689133a7b3b81c2d54c78ff14bcf
Daisuke Aoki <aoki@tac-netweb.co.jp>
2026-01-30 18:21:00 +0900

    totyuu

---
+++ src/components/ScoreParts.fs
@@ -0,0 +1,33 @@
+﻿module App.components.ScoreParts
+
+open System.Linq
+open Feliz
+
+let beatCell (beatIndex: int) =
+    Html.div [
+        prop.style [
+            style.width 40
+            style.height 40
+            style.border (1, borderStyle.solid, "gray")         
+            style.display.flex
+            style.alignItems.center
+            style.justifyContent.center
+            style.cursor.pointer
+        ]
+        prop.text (string beatIndex)
+        prop.onClick (fun _ -> printfn $"Clicked beat {beatIndex}")        
+    ]
+    
+let bar (beatsPerMeasure: int) =
+    Html.div [
+        prop.style [
+            style.display.flex
+            style.gap 2
+            style.marginBottom 4
+        ]
+        
+        prop.children [
+            Seq.map ()
+        ]
+    ]
+    
